ccab3c47f02c7c10a6c24a8d2f714e452074c70d
Blednaya Luna <alexander.shandurenko@gmail.com>
2025-12-06 23:25:32 +0100

    StackArrayWithSet

---
+++ StackArrayWithSet.ts
@@ -0,0 +1,56 @@
+class StackArrayWithSet<T> {
+    private items: T[];
+    private set: Set<T>;
+
+    constructor(items: readonly T[] = []) {
+        this.items = [...items];
+        this.set = new Set(items);
+    }
+    
+    push(item: T): void {
+        this.items.push(item);
+        this.set.add(item);
+    }
+    
+    pop(): T | undefined {
+        const item = this.items.pop();
+        if (item !== undefined) this.set.delete(item);
+        return item;
+    }
+    
+    peek(): T | undefined {
+        return this.items[this.items.length - 1];
+    }
+    
+    isEmpty(): boolean {
+        return this.size() === 0;
+    }
+
+    size(): number {
+        return this.items.length;
+    }    
+
+    clear(): void {
+        this.items = [];
+        this.set = new Set();
+    }
+
+    contains(item: T, equalsFn?: (a: T, b: T) => boolean): boolean {
+        if (equalsFn) {
+            return this.items.some((value) => equalsFn(value, item));
+        }
+        return this.set.has(item);
+    }
+
+    toArray() {
+        return [...this.items];
+    }
+
+    static fromArray<U>(array: readonly U[]): StackArrayWithSet<U> {
+        return new StackArrayWithSet(array);
+    }
+
+    clone(): StackArrayWithSet<T> {
+        return new StackArrayWithSet(this.items);
+    }
+};
